0ef26ee1ff2513355ef2bcc35dfbbe390e09e6b0
Avi Kaufman <avi_p_k@yahoo.com>
2017-12-28 16:33:32 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      	<h1>Hello Bulls and Cows</h1>
+      	<figure>
+      		<img src="images/logo.png"/>
+  		</figure>
+      </div>
+    );
+  }
+}
+
+export default App;
